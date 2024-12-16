ff26d5058deb710882a755443f151800f6b85022
Kerr Cameron <90578826+KERRCAM@users.noreply.github.com>
2024-12-16 18:03:42 +0000

    json load to string

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ There is likely better alternatives but this is a library for parsing JSON files
 I personally have had issues doing this with other libraries on apple silicon so this aims to fill that gap using nothing system specific.
 
 Should result in less features but a more lightweight and more portable library for using JSON with C.
+
--- jsonParse.c
+++ jsonParse.c
@@ -1,6 +1,46 @@
 #include "jsonParse.h"
+#include <stdlib.h>
+
+
+
+char* loadJSON(char* filename){
+
+    char * buffer = 0;
+    long length;
+    FILE * fp = fopen (filename, "rb");
+    char* jsonContent;
+
+    if (fp){
+        fseek (fp, 0, SEEK_END);
+        length = ftell (fp);
+        fseek (fp, 0, SEEK_SET);
+        buffer = malloc (length);
+        if (buffer){
+            fread (buffer, 1, length, fp);
+        }
+        fclose (fp);
+    }
+
+    if (buffer){
+        jsonContent = buffer;
+    }
+
+    return jsonContent;
+
+}
+
+
+
+
+
 
 int main(){
+
+    char* filename = "test.json";
+
+    char* jsonContent = loadJSON(filename);
+
+    printf("%s\n", jsonContent);
 
     return 0;
 
